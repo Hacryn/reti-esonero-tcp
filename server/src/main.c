02ed94a8502a4d16d84471c6aa467472ab5bd4df
Enrico Ciciriello <Hacryn@users.noreply.github.com>
2021-11-11 16:03:38 +0100

    Project setup

---
+++ server/src/main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Enrico Ciciriello -  Simone Summo
+ Version     :
+ Copyright   : MIT
+ Description : Online calculator (server)
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!"); /* prints Hello World! */
+	return EXIT_SUCCESS;
+}
