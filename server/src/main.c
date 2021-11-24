d77d33056d060b7efab44f779c1155569f5005de
Enrico Ciciriello <Hacryn@users.noreply.github.com>
2021-11-24 17:27:34 +0100

    Fixed port selection 2

--- server/src/main.c
+++ server/src/main.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]){
 		port = atoi(argv[1]);
 	} else {
 		port = PORT;
-		if(port < 0){
+		if(port < 1 || port > 65535){
 			printf("bad port number %s \n", argv[1]);
 			return 0;
 		}
